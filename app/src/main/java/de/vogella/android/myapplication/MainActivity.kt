a1e418ab6e93f7200ca506c79659c57bdb35b5f5
chun <chunjai369@gmail.com>
2021-10-31 16:00:05 +0800

    mainActivity add check connect and version , requsetQueueu_manager add new requset

--- app/src/main/java/de/vogella/android/myapplication/MainActivity.kt
+++ app/src/main/java/de/vogella/android/myapplication/MainActivity.kt
@@ -5,14 +5,12 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import com.android.volley.RequestQueue
+import androidx.appcompat.app.AlertDialog
 import com.android.volley.Response
-import com.android.volley.toolbox.Volley
 import de.vogella.android.myapplication.components.requestQueue_Manager
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var queue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,28 +19,65 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
+        val req = requestQueue_Manager(this)
         val url = "http://10.0.2.2:3001/"
-        queue = Volley.newRequestQueue(this)
-        Handler(Looper.getMainLooper()).post{
-            Log.v("showLog","start")
-            val pref = getSharedPreferences("bookkeeping",0)
-            val token = pref.getString("token", "noToken")
-            if (token == "noToken") {
-                goLogin()
-            }else{
-                val req = requestQueue_Manager(this)
-                req.Request("post",url,null,Response.Listener<JSONObject>{ res ->
-                    val isVerify = res.getBoolean("verify")
-                    if(isVerify){
-                        startActivity(Intent(this, mainpage::class.java))
-                        finish()
-                    }else{
-                        pref.edit().remove("token").commit()
-                        goLogin()
-                    }
+        var overConnect = false
+        var isConnect = false
+        var oldVersion = false
+        Handler(Looper.getMainLooper()).postDelayed({
+            if(!isConnect) {
+                overConnect = true
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(R.string.error)
+                builder.setMessage(R.string.Connect_time_out)
+                builder.setPositiveButton("Confirm",{ dialog, whichButton ->
+                    System.exit(0)
                 })
+                val dialog = builder.create()
+                dialog.show()
             }
+        },10000)
+
+        Handler(Looper.getMainLooper()).post{
+            req.Request("get",url,Response.Listener<JSONObject>{ res ->
+                isConnect = res.getBoolean("is_connect")
+                if(isConnect && !overConnect){
+                    val pref = getSharedPreferences("bookkeeping",0)
+                    val token = pref.getString("token", "noToken")
+                    val version = pref.getString("version", "noVersion")
+                    if (version == "noVersion"){
+                        pref.edit().putString("version",res.getString("Version")).apply()
+                    }else{
+                        if (version != res.getString("Version")){
+                            oldVersion = true
+                            val builder = AlertDialog.Builder(this)
+                            builder.setTitle(R.string.error)
+                            builder.setMessage(R.string.update_version)
+                            builder.setPositiveButton("Confirm",{ dialog, whichButton ->
+                                System.exit(0)
+                            })
+                            val dialog = builder.create()
+                            dialog.show()
+                        }
+                    }
+                    if(!oldVersion){
+                        if (token == "noToken") {
+                            goLogin()
+                        }else{
+                            req.Request("post",url,null,Response.Listener<JSONObject>{ res ->
+                                val isVerify = res.getBoolean("verify")
+                                if(isVerify){
+                                    startActivity(Intent(this, mainpage::class.java))
+                                    finish()
+                                }else{
+                                    pref.edit().remove("token").commit()
+                                    goLogin()
+                                }
+                            })
+                        }
+                    }
+                }
+            })
         }
     }
 
--- app/src/main/java/de/vogella/android/myapplication/components/requestQueue_Manager.kt
+++ app/src/main/java/de/vogella/android/myapplication/components/requestQueue_Manager.kt
@@ -6,6 +6,7 @@ import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONArray
 import org.json.JSONObject
@@ -15,6 +16,21 @@ class requestQueue_Manager(context: Context) {
     private val pref = context?.getSharedPreferences("bookkeeping",0)
     private val token = pref?.getString("token", "noToken")
     private lateinit var queue: RequestQueue
+
+    fun Request(Method:String,url:String,resF :Response.Listener<JSONObject>){
+        queue = Volley.newRequestQueue(context)
+        val req = JsonObjectRequest(when(Method){
+            "get" -> Request.Method.GET
+            "post" -> Request.Method.POST
+            "patch" -> Request.Method.PATCH
+            else -> Request.Method.DELETE
+        },
+            url,
+            null,
+            resF,
+            {error -> Log.e("showLog", error.toString())})
+        queue.add(req)
+    }
 
     fun Request(Method:String,url:String,jsonObj:JSONObject?,resF : Response.Listener<JSONObject>){
 
