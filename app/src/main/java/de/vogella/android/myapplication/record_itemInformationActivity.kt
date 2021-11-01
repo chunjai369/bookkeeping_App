ad3e4682c599f492d048ec87e0cbb1e7277c87bd
chun <chunjai369@gmail.com>
2021-11-01 16:41:19 +0800

    create record item activity View

--- app/src/main/java/de/vogella/android/myapplication/record_itemInformationActivity.kt
+++ app/src/main/java/de/vogella/android/myapplication/record_itemInformationActivity.kt
@@ -3,12 +3,15 @@ package de.vogella.android.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class record_itemInformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_item_information)
         val intent = getIntent()
-        val data = intent.getStringExtra("date")
+        val date = intent.getStringExtra("date")
+        val dateTextView = findViewById(R.id.item_info_date) as TextView
+        dateTextView.setText(date)
     }
 }
