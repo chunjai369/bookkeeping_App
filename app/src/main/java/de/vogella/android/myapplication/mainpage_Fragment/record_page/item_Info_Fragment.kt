2b959e08b304fa4f78cb5c04bfec6d1d436e37cc
chun <chunjai369@gmail.com>
2021-11-02 19:59:14 +0800

    change item_item view

--- app/src/main/java/de/vogella/android/myapplication/mainpage_Fragment/record_page/item_Info_Fragment.kt
+++ app/src/main/java/de/vogella/android/myapplication/mainpage_Fragment/record_page/item_Info_Fragment.kt
@@ -3,13 +3,11 @@ package de.vogella.android.myapplication.mainpage_Fragment.record_page
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.view.size
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,8 +19,7 @@ import org.json.JSONArray
 import org.json.JSONObject
 
 class item_Info_Fragment : Fragment() {
-    private val url1 = "http://10.0.2.2:3001/trade/income_expend?type=income&date="
-    private val url2 = "http://10.0.2.2:3001/trade/income_expend?type=expend&date="
+    private val url = "http://10.0.2.2:3001/trade/income_expend?date="
     private lateinit var  date : String
     private lateinit var  position : String
     override fun onCreateView(
@@ -33,18 +30,16 @@ class item_Info_Fragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_item_info, container, false)
         date = this.requireArguments().getString("date").toString()
         position = this.requireArguments().getString("position").toString()
-        var data1 : ArrayList<ArrayList<String>>
-        var data2 : ArrayList<ArrayList<String>>
+        var data : ArrayList<ArrayList<String>>
         val dateTextView = root.findViewById(R.id.item_info_date) as TextView
         dateTextView.setText(date)
         val manager = activity?.supportFragmentManager
-        val recyclerView_income = root.findViewById<RecyclerView>(R.id.recyclerview_income)
-        val recyclerView_expend = root.findViewById<RecyclerView>(R.id.recyclerview_expend)
+        val recyclerView_income = root.findViewById<RecyclerView>(R.id.recyclerview)
         val del_btn = root.findViewById<ImageView>(R.id.del_date)
         val requestManage = context?.let { requestQueue_Manager(it) }
 
         del_btn.setOnClickListener{ v->
-            val url = "http://10.0.2.2:3001/trade?type=date&date=$date"
+            val url = "http://10.0.2.2:3001/trade?date=$date"
             val requestManage = requestQueue_Manager(v.context)
             requestManage.Request("delete",url,null,Response.Listener<JSONObject>{ res ->
                 if (res.getBoolean("is_delete")) {
@@ -56,21 +51,14 @@ class item_Info_Fragment : Fragment() {
             })
         }
 
-        requestManage?.Request("get",url1+date,null,Response.Listener<JSONArray>{  res->
+        requestManage?.Request("get",url+date,null,Response.Listener<JSONArray>{  res->
             if(res.length() != 0){
-                data1 = changeData(res)
+                data = changeData(res)
                 recyclerView_income.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-                recyclerView_income.adapter = manager?.let { item_Adapter(it, this,true,data1) }
+                recyclerView_income.adapter = manager?.let { item_Adapter(it, this,true,data) }
             }
         })
 
-        requestManage?.Request("get",url2+date,null,Response.Listener<JSONArray>{  res->
-            if(res.length() != 0){
-                data2 = changeData(res)
-                recyclerView_expend.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-                recyclerView_expend.adapter = manager?.let { item_Adapter(it, this,false,data2) }
-            }
-        })
         return root
     }
 
